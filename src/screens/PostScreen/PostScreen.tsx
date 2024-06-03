0c81552be77672ba9035540cee39eb1e1d9dd47b
willperes <thewidocs@gmail.com>
2024-06-03 16:55:01 -0300

    fix: screen safe area cutting post screen scroll view bottom

--- src/screens/PostScreen/PostScreen.tsx
+++ src/screens/PostScreen/PostScreen.tsx
@@ -5,20 +5,21 @@ import {PostElement, usePostDetails} from '@/domain';
 import {FlatList, ListRenderItemInfo} from 'react-native';
 import {PostHeader} from './components/PostHeader';
 import {PostBodyElement} from './components/PostBodyElement';
-import {useAppTheme} from '@/hooks';
+import {useAppSafeArea, useAppTheme} from '@/hooks';
 
 export function PostScreen({route}: AppScreenProps<'PostScreen'>) {
   const {postID} = route.params;
 
   const {data: post} = usePostDetails(postID);
   const {spacing} = useAppTheme();
+  const {bottom} = useAppSafeArea();
 
   function renderItem({item}: ListRenderItemInfo<PostElement>) {
     return <PostBodyElement {...item} />;
   }
 
   return (
-    <Screen canGoBack noHorizontalPading>
+    <Screen canGoBack noHorizontalPading style={{paddingBottom: 0}}>
       {!post ? (
         <Text>{`postID: ${postID}`}</Text>
       ) : (
@@ -29,6 +30,7 @@ export function PostScreen({route}: AppScreenProps<'PostScreen'>) {
           contentContainerStyle={{
             flexGrow: 1,
             paddingHorizontal: spacing.screenPadding,
+            paddingBottom: bottom,
             gap: spacing.s12,
           }}
           showsVerticalScrollIndicator={false}
